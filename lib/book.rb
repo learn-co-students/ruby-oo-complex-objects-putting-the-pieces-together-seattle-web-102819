854d7fe0995e1754f9c955e637bf7fa9ec5358ef
4myc <missch3n@gmail.com>
2019-11-25 13:42:59 -0800

    done

--- lib/book.rb
+++ lib/book.rb
@@ -9,27 +9,40 @@ class Book
     def turn_page
         puts "Flipping the page...wow, you read fast!"
     end
-end
+
+    # ADDING attr_accessor eliminates need to write the setters and getters below
     
+    # def title=(title)
+    #     @title = title 
+    # end
+
     # def title
     #     @title
     # end
+
     # def author=(author)
     #     @author = author
     # end
+
     # def author
     #     @author
     # end
+
     # def page_count=(page_count)
     #     @page_count = page_count
     # end
+
     # def page_count
     #     @page_count
     # end
+
     # def genre=(genre)
     #     @genre = genre
     # end
+
     # def genre
     #     @genre
     # end
+end
+
 
